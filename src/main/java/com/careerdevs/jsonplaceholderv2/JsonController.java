4ddca5f3249063cdff93f65aa5d76469b1320c45
darielhernandez <13dariel@gmail.com>
2022-05-05 21:34:05 -0400

    JSON Placeholder completed and working at every step.

--- src/main/java/com/careerdevs/jsonplaceholderv2/JsonController.java
+++ src/main/java/com/careerdevs/jsonplaceholderv2/JsonController.java
@@ -1,8 +1,7 @@
 package com.careerdevs.jsonplaceholderv2;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
@@ -10,7 +9,53 @@ import org.springframework.web.bind.annotation.RestController;
 public class JsonController {
 
     @GetMapping("/test")
-    public String test(){
+    public String test() {
         return "Test worked!";
     }
+
+    @GetMapping("/getAllPosts")
+    public Object getPosts(RestTemplate restTemplate) {
+        return restTemplate.getForObject("https://jsonplaceholder.typicode.com/posts/", Object.class);
+    }
+
+    @GetMapping("/getPostById/{id}")
+    public Object getPostById(RestTemplate restTemplate, @PathVariable String id) {
+        return restTemplate.getForObject("https://jsonplaceholder.typicode.com/posts/" + id, Object.class);
+    }
+
+    @GetMapping("posts/{id}/comments")
+    public Object getCommentsByPost(RestTemplate restTemplate, @PathVariable String id){
+        return restTemplate.getForObject("https://jsonplaceholder.typicode.com/posts/" + id + "/comments", Object.class);
+    }
+
+    @GetMapping("/comments")
+    public Object getCommentsByPostQuery(RestTemplate restTemplate, @RequestParam String id){
+        return restTemplate.getForObject("https://jsonplaceholder.typicode.com/comments?postId=" + id, Object.class);
+    }
+
+    @PostMapping("/posts")
+    public Post createPost(RestTemplate restTemplate, @RequestBody Post newPost){
+        /*
+        postForObject takes 3 args
+        * 1. URL
+        * 2. Request body
+        * 3. Return type
+        */
+        return restTemplate.postForObject("https://jsonplaceholder.typicode.com/posts/", newPost, Post.class);
+    }
+
+    @PutMapping("posts/{id}")
+    public String updatePost(RestTemplate restTemplate, @PathVariable int id, @RequestBody Post post) {
+        restTemplate.put("https://jsonplaceholder.typicode.com/posts/" + id, post);
+        return "Post with id " + id + "successfully updated";
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public String deletePost(RestTemplate restTemplate, @PathVariable int id){
+        restTemplate.delete("https://jsonplaceholder.typicode.com/posts/" + id);
+        return "Deleted post with ID " + id;
+    }
+
 }
+
+
